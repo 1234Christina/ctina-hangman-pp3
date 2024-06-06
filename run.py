64cf366b4ff9ffdff13087ce7d668cdbf8e0918f
1234Christina <ctina89h@gmail.com>
2024-06-06 20:46:20 +0000

    Extend word bank

--- run.py
+++ run.py
@@ -8,7 +8,13 @@ Hangman Game Project for PP3
 
 # A word bank from which the words will be chosen randomly
 # for the user to guess
-word_bank = ['apple', 'ball', 'cat', 'duck']
+word_bank = ['apple', 'ball', 'cat', 'duck', 'eggshell', 'football', 'garden',
+'harp', 'icecream', 'jam', 'king', 'library', 'museum', 'nutella', 'ocean',
+'playground', 'queen', 'race', 'snake', 'tea', 'under', 'vulture',
+'waterfall', 'xylophone', 'yoyo', 'zoologist', 'always', 'before', 'chapel',
+'dragon', 'electricity', 'fudge', 'game', 'heart', 'ice', 'jersey', 'kitten', 
+'lightbulb', 'monsoon', 'nut', 'octagon', 'pretend', 'quick', 'ready',
+'smooth', 'train', 'unbelievable', 'van', 'wheel', 'xray', 'yard', 'zip']
 
 """ 
 Welcome the user to Hangman
@@ -16,3 +22,4 @@ Welcome the user to Hangman
 print("Welcome to Hangman")
 
 # print(word_bank[2])
+print(word_bank[-34])
