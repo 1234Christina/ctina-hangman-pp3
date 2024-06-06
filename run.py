58d1eae9f2a04e60d5a8b0aebf3985087d5ca938
1234Christina <ctina89h@gmail.com>
2024-06-06 20:02:24 +0000

    Introduce word bank for Hangman game

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+""" 
+Hangman Game Project for PP3
+"""
+
+# A word bank from which the words will be chosen randomly
+# for the user to guess
+word_bank = ['apple', 'ball', 'cat', 'duck']
+
+""" 
+Welcome the user to Hangman
+"""
+print("Welcome to Hangman")
+
+# print(word_bank[2])
