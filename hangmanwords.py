109e4b5401a6280e07a4317caf02a6a7942d40a9
1234Christina <ctina89h@gmail.com>
2024-06-10 09:14:55 +0000

    Move word bank to file and import it in run.py

---
+++ hangmanwords.py
@@ -0,0 +1,14 @@
+# A word bank from which the words will be chosen randomly
+# for the user to guess
+
+word_bank = ['apple', 'ball', 'cat', 'duck', 'eggshell', 'football', 'garden', 
+'harp', 'icecream', 'jam', 'king', 'library', 'museum', 'nutella', 'ocean',
+'playground', 'queen', 'race', 'snake', 'tea', 'under', 'vulture',
+'waterfall', 'xylophone', 'yoyo', 'zoologist', 'always', 'before', 'chapel',
+'dragon', 'electricity', 'fudge', 'game', 'heart', 'ice', 'jersey', 'kitten',
+'lightbulb', 'monsoon', 'nut', 'octagon', 'pretend', 'quick', 'ready',
+'smooth', 'train', 'unbelievable', 'van', 'wheel', 'xray', 'yard', 'zip',
+'abacus', 'blackboard', 'chalk', 'dusty', 'emoji', 'flamingo', 'goblin',
+'halloween', 'island', 'joker', 'ketchup', 'lookalike', 'magic', 'newspaper',
+'office', 'priest', 'qualify', 'rabbit', 'shop', 'turtle', 'uncle', 'video',
+'wagon', 'xbox', 'yoghurt', 'zero']
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random 
+from hangmanwords import word_bank
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
@@ -7,19 +8,19 @@ import random
 Hangman Game Project for PP3
 """
 
-# A word bank from which the words will be chosen randomly
-# for the user to guess
-word_bank = ['apple', 'ball', 'cat', 'duck', 'eggshell', 'football', 'garden',
-'harp', 'icecream', 'jam', 'king', 'library', 'museum', 'nutella', 'ocean',
-'playground', 'queen', 'race', 'snake', 'tea', 'under', 'vulture',
-'waterfall', 'xylophone', 'yoyo', 'zoologist', 'always', 'before', 'chapel',
-'dragon', 'electricity', 'fudge', 'game', 'heart', 'ice', 'jersey', 'kitten',
-'lightbulb', 'monsoon', 'nut', 'octagon', 'pretend', 'quick', 'ready',
-'smooth', 'train', 'unbelievable', 'van', 'wheel', 'xray', 'yard', 'zip',
-'abacus', 'blackboard', 'chalk', 'dusty', 'emoji', 'flamingo', 'goblin',
-'halloween', 'island', 'joker', 'ketchup', 'lookalike', 'magic', 'newspaper',
-'office', 'priest', 'qualify', 'rabbit', 'shop', 'turtle', 'uncle', 'video',
-'wagon', 'xbox', 'yoghurt', 'zero']
+# # A word bank from which the words will be chosen randomly
+# # for the user to guess
+# word_bank = ['apple', 'ball', 'cat', 'duck', 'eggshell', 'football', 'garden',
+# 'harp', 'icecream', 'jam', 'king', 'library', 'museum', 'nutella', 'ocean',
+# 'playground', 'queen', 'race', 'snake', 'tea', 'under', 'vulture',
+# 'waterfall', 'xylophone', 'yoyo', 'zoologist', 'always', 'before', 'chapel',
+# 'dragon', 'electricity', 'fudge', 'game', 'heart', 'ice', 'jersey', 'kitten',
+# 'lightbulb', 'monsoon', 'nut', 'octagon', 'pretend', 'quick', 'ready',
+# 'smooth', 'train', 'unbelievable', 'van', 'wheel', 'xray', 'yard', 'zip',
+# 'abacus', 'blackboard', 'chalk', 'dusty', 'emoji', 'flamingo', 'goblin',
+# 'halloween', 'island', 'joker', 'ketchup', 'lookalike', 'magic', 'newspaper',
+# 'office', 'priest', 'qualify', 'rabbit', 'shop', 'turtle', 'uncle', 'video',
+# 'wagon', 'xbox', 'yoghurt', 'zero']
 
 
 def get_random_word(word_bank):
@@ -88,6 +89,9 @@ dashes_for_words()
 
 # print(word_bank[2])
 # print(word_bank[-34])
+# print(word_bank[-32])
+# print(word_bank[42])
+# print(word_bank[12])
 
 # dashes_for_words()
 # get_random_word(word_bank)
