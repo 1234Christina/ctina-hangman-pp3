8c867bb9e6cd1cf47e762a252c4bf82887ea1d5d
1234Christina <ctina89h@gmail.com>
2024-06-12 12:02:36 +0000

    Debug: Refine check guess function to add guess to list of guesses

--- run.py
+++ run.py
@@ -54,13 +54,16 @@ def check_guess():
     # current_word = get_random_word(word_bank)
     # guessed_letter = get_user_guess()
 
-    print('LINE 57', guessed_letters)
+    print('LIST of guessed letters:', guessed_letters)
     guessed_letter = guessed_letters.pop()
+    print('guessed letter:', guessed_letter)
 
     if guessed_letter in current_word:
         print('You guessed a correct letter!')
     else:
         print('Try again')
+    
+
 
 """ 
 Welcome the user to Hangman
@@ -84,13 +87,17 @@ def main():
     """
     A function to run all game functions
     """
+    global guessed_letters
+    guessed_letters = []
+    
+    global current_word
     current_word = get_random_word(word_bank)
-    print(current_word)
+    # print(current_word)
     dashes_for_words(current_word)
 
     while True:
 
-        user_guess = input('Type your guess here: \n').upper()
+        user_guess = input('Type your guess here: \n').strip().upper()
         # print(user_guess)
         print(f'You guessed {user_guess}')
         print('Checking answer...')
@@ -103,16 +110,21 @@ def main():
             print('Please enter just one letter')
             continue
 
+        if user_guess in guessed_letters:
+            print('You have already guessed that letter, try again!')
+            continue
+
         break
 
 
-    global guessed_letters
-    guessed_letters = []
+    # global guessed_letters
+    # guessed_letters = []
     guessed_letters.append(user_guess)
-    print('LINE 111', guessed_letters)
+    # print('LINE 119', guessed_letters)
+
 
     # guessed_letter = guessed_letters.pop()
-    print(guessed_letter)
+    # print(guessed_letter)
     check_guess()
 
 main()
