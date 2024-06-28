19f25d6e3e6cb571c07d7cc754f28beef00fef8f
Christina Hanlon <ctina89h@gmail.com>
2024-06-28 10:53:33 +0000

    Update check guess function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import random  # Better to use this or a random index from word bank list?
+import random 
 from hangmanwords import word_bank
 
 """ 
@@ -9,9 +9,7 @@ def get_random_word(word_bank):
     """ 
     A function to get a random word from the word bank
     """
-#     global random_word
     random_word = random.choice(word_bank).upper()
-    # print('CAPITAL RANDOM WORD:', random_word)
     print(' ')
     return random_word
 
@@ -22,14 +20,8 @@ def dashes_for_words(word):
     in the randomly chosen word
     """
     print('_ ' * len(word))
-    for letter in random_word:
-        if letter in letters_to_reveal:
-            print(letter.upper() + ' ', end= ' ')
 
-        else:
-            print('_ ', end='')
-
-def check_guess(word):
+def check_guess():
     """
     User inputs their guess and this input is validated
     Check if users guessed letter is in the word and give feedback 
@@ -57,44 +49,27 @@ def check_guess(word):
             print('You have already guessed that letter, try again!')
             continue
 
-        break
+        continue
 
+    print('LIST of guessed letters:', guessed_letters)
+    guessed_letter = guessed_letters[-1]
+    print('guessed letter:', guessed_letter)
 
-    # print('LIST of guessed letters:', guessed_letters)
-    # guessed_letter = guessed_letters[-1]
-    # print('guessed letter:', guessed_letter)
-
-    # if guessed_letter in current_word:
-    #     print('You guessed a correct letter! Well done!')
-
-    #     # Reveal the word, with dashes and correctly guessed letters
-    #     print('')
-
-    #     # Get the users input again 
-    # else:
-    #     print('Keep Trying!')
-
-    #     # Add the hangman drawing here
-    #     print('Add the hangman drawing here')    
+    if guessed_letter in current_word:
+        print('You guessed a correct letter! Well done!')
+    else:
+        print('Keep Trying!')
 
 def main():
     """
-    A function to run all game functions
+    Run all game functions
     """
-
-    # Welcome the user to Hangman
 
     user_name = input('To Begin please enter your name: \n').upper().strip()
     print(f"Welcome to Hangman {user_name}! \nHere is your first word:")
 
-#     global guessed_letters
-#     guessed_letters = []
-    
-#     global current_word
+    guessed_letters = []
     current_word = get_random_word(word_bank)
-    # print('CURRENT_WORD:', current_word)
-
-#     letters_to_reveal = 'c,a,t'
     dashes_for_words(current_word)
 
     keep_playing = True
@@ -104,7 +79,6 @@ def main():
             """ User inputs their guess and this input is validated """
 
             user_guess = input('Type your guess here: \n').strip().upper()
-            # print(user_guess)
             print(f'You guessed {user_guess}')
             print('')
             print('Checking answer...')
@@ -122,12 +96,8 @@ def main():
                 continue
 
             break
+        check_guess()
 
-        # guessed_letters.append(user_guess)
-        return True
-    
         guessed_letters.append(user_guess)
 
-    guess_is_correct = check_guess(current_word) # Must use the letter and word now
-#     # keep_playing = False
 main()
