2041f19d5553fd30f2b14353a0362510d31b15a9
Christina Hanlon <ctina89h@gmail.com>
2024-06-28 09:46:38 +0000

    Add initial sections and intro to README.md

--- run.py
+++ run.py
@@ -9,17 +9,19 @@ def get_random_word(word_bank):
     """ 
     A function to get a random word from the word bank
     """
-    global random_word
+#     global random_word
     random_word = random.choice(word_bank).upper()
-    print('CAPITAL RANDOM WORD:', random_word)
+    # print('CAPITAL RANDOM WORD:', random_word)
+    print(' ')
     return random_word
 
-def dashes_for_words(random_word, letters_to_reveal):
+
+def dashes_for_words(word):
     """ 
     A function to print dashes for the number of letters 
     in the randomly chosen word
     """
-
+    print('_ ' * len(word))
     for letter in random_word:
         if letter in letters_to_reveal:
             print(letter.upper() + ' ', end= ' ')
@@ -27,10 +29,36 @@ def dashes_for_words(random_word, letters_to_reveal):
         else:
             print('_ ', end='')
 
-def check_guess(word, guessed_letter):
+def check_guess(word):
     """
-    Find out if users guessed letter is in the word and give feedback
+    User inputs their guess and this input is validated
+    Check if users guessed letter is in the word and give feedback 
     """
+
+    user_guess = input('Type your guess here: \n').strip().upper()
+
+    print(f'You guessed {user_guess}')
+    print('')
+    print('Checking answer...')
+    
+    while True:
+        
+        if not user_guess.isalpha():
+            print('Please enter a letter')
+            continue
+
+        if len(user_guess) > 1:
+            print('Please enter just one letter')
+            continue
+
+        guessed_letters.append(user_guess)
+
+        if user_guess in guessed_letters:
+            print('You have already guessed that letter, try again!')
+            continue
+
+        break
+
 
     print('LIST of guessed letters:', guessed_letters)
     guessed_letter = guessed_letters[-1]
@@ -56,18 +84,18 @@ def main():
 
     # Welcome the user to Hangman
 
-    user_name = input('To Begin please enter your name: \n').upper()
+    user_name = input('To Begin please enter your name: \n').upper().strip()
     print(f"Welcome to Hangman {user_name}! \nHere is your first word:")
 
-    global guessed_letters
-    guessed_letters = []
+#     global guessed_letters
+#     guessed_letters = []
     
-    global current_word
+#     global current_word
     current_word = get_random_word(word_bank)
-    print('CURRENT_WORD:', current_word)
+    # print('CURRENT_WORD:', current_word)
 
-    letters_to_reveal = 'c,a,t'
-    dashes_for_words(current_word, letters_to_reveal)
+#     letters_to_reveal = 'c,a,t'
+    dashes_for_words(current_word)
 
     keep_playing = True
     while keep_playing:
@@ -100,6 +128,6 @@ def main():
     
         guessed_letters.append(user_guess)
 
-    guess_is_correct = check_guess(current_word, user_guess) # Must use the letter and word now
-    # keep_playing = False
+    guess_is_correct = check_guess(current_word) # Must use the letter and word now
+#     # keep_playing = False
 main()
