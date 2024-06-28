dabd42d2051fbc41ec3d83890c84969a48506b5e
Christina Hanlon <ctina89h@gmail.com>
2024-06-28 11:38:44 +0000

    delete unused code

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def check_guess():
     User inputs their guess and this input is validated
     Check if users guessed letter is in the word and give feedback 
     """
-
+    guessed_letters = []
     user_guess = input('Type your guess here: \n').strip().upper()
 
     print(f'You guessed {user_guess}')
@@ -43,22 +43,18 @@ def check_guess():
             print('Please enter just one letter')
             continue
 
-        guessed_letters.append(user_guess)
-
         if user_guess in guessed_letters:
             print('You have already guessed that letter, try again!')
             continue
 
-        continue
+        break
 
-    print('LIST of guessed letters:', guessed_letters)
+    guessed_letters.append(user_guess)
     guessed_letter = guessed_letters[-1]
-    print('guessed letter:', guessed_letter)
 
-    if guessed_letter in current_word:
-        print('You guessed a correct letter! Well done!')
-    else:
-        print('Keep Trying!')
+    return guessed_letter
+
+#new function
 
 def main():
     """
@@ -68,36 +64,8 @@ def main():
     user_name = input('To Begin please enter your name: \n').upper().strip()
     print(f"Welcome to Hangman {user_name}! \nHere is your first word:")
 
-    guessed_letters = []
     current_word = get_random_word(word_bank)
     dashes_for_words(current_word)
-
-    keep_playing = True
-    while keep_playing:
-
-        while True:
-            """ User inputs their guess and this input is validated """
-
-            user_guess = input('Type your guess here: \n').strip().upper()
-            print(f'You guessed {user_guess}')
-            print('')
-            print('Checking answer...')
-
-            if not user_guess.isalpha():
-                print('Please enter a letter')
-                continue
-
-            if len(user_guess) > 1:
-                print('Please enter just one letter')
-                continue
-
-            if user_guess in guessed_letters:
-                print('You have already guessed that letter, try again!')
-                continue
-
-            break
-        check_guess()
-
-        guessed_letters.append(user_guess)
+    check_guess()
 
 main()
